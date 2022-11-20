ba5d83f5410a53fcc12fdf061cd4fe532f843e01
ederjbbb <ederjb@icloud.com>
2022-11-20 20:17:49 +0000

    in progerss

---
+++ src/components/Loader/styles.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components/native";
+
+
+export const Container = styled.View`
+    flex:1;
+    justify-content: center;
+    align-items: center;
+    background: ${props => props.theme.COLORS.GRAY_600};
+`;
+
+
